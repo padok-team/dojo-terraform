8f95128a3fd151856dcdf46ec0ab5cafb688f5b4
blblbly <lilac@padok.fr>
2023-04-29 12:47:14 +0000

    Creation of new files

--- exercises/app/_settings.tf
+++ exercises/app/_settings.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_providers >= 4.0.0 {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "> 4.0.0"
     }
   }
 }
